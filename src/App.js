a6931d278bb5f82d108c030105fd6e99bcc46963
ishan <ishan.hcl.tab1@gmail.com>
2020-11-30 00:49:31 +0530

    Add Buger builder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Layout from './components/Layout/Layout'
-
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 function App() {
   return (
     <div >
       <Layout>
-        <p>Test</p>
+        <BurgerBuilder></BurgerBuilder>
       </Layout>
     </div>
   );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 const Layout = (props) =>(
     <Aux>
         <div>
             toolbar ,sidebar,backdrop
         </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
 
