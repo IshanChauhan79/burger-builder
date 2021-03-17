93f93d5986c064eb114092b9cce56ce5e876758f
ishan <ishan.hcl.tab1@gmail.com>
2021-03-18 01:19:21 +0530

    Add optimization and bug fix

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -56,7 +56,7 @@ export const auth = (email,password,isSignUp)=>{
         }
         axios.post(url,authData)
             .then(response=>{
-                console.log(response);
+                // console.log(response);
                 const expirationDate= new Date(new Date().getTime() + response.data.expiresIn*1000)
                 localStorage.setItem('token',response.data.idToken);
                 localStorage.setItem('expirationDate',expirationDate);
@@ -65,7 +65,7 @@ export const auth = (email,password,isSignUp)=>{
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err=>{
-                console.log(err);
+                // console.log(err);
                 dispatch(authFail(err.response.data.error));
             })
             
