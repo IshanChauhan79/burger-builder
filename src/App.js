44743e41f3957183677505af83839f7612193495
ishan <ishan.hcl.tab1@gmail.com>
2021-03-18 00:32:39 +0530

    Add routes protection and sort orders by user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import {Route,Switch,withRouter} from 'react-router-dom';
+import {Route,Switch,withRouter,Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import * as actions from './store/actions/index';
 import Layout from './hoc/Layout/Layout'
@@ -10,25 +10,42 @@ import Orders from './containers/Orders/Orders';
 import Logout from './containers/Auth/Logout/Logout';
 import Auth from './containers/Auth/Auth';
 class App extends Component {
-  componentDidMount(){
+  componentWillMount(){
     this.props.onTryAutoSignUp();
   }
   render(){
+    let routes=(
+      <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path='/logout' component={Logout} />
+          <Route path="/"  exact component={BurgerBuilder} /> 
+          <Redirect to="/" />
+      </Switch>
+    )
+    if (this.props.isAuthenticated){
+      routes=(
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path='/logout' component={Logout} />
+          <Route path="/"  exact component={BurgerBuilder} /> 
+          <Redirect to='/' />
+      </Switch>
+      )
+    }
+
     return (
       <div >
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path='/logout' component={Logout} />
-
-            <Route path="/"  exact component={BurgerBuilder} />
-            
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
+  }
+}
+const mapStateToProps = state =>{
+  return{
+    isAuthenticated:state.auth.token !== null
   }
 }
 
@@ -38,6 +55,4 @@ const mapDispatchToProps = dispatch=>{
   }
 }
 
-export default withRouter(connect(null,mapDispatchToProps)(App));
-{/* <BurgerBuilder />
-<Checkout /> */}
+export default withRouter(connect(mapStateToProps,mapDispatchToProps)(App));
